e81e7b16e2c0c4895793a25c2e40c247898b7f61
Pawan Krishna Shrestha <pkshrestha@sevadev.com>
2018-11-28 14:16:53 +0545

    added scripts for GRID, killing chromedriver and geckodriver processes

---
+++ scripts/hubstart.bat
@@ -0,0 +1,1 @@
+java -jar src/test/java/resources/drivers/standalone/selenium-server-standalone-3.141.59.jar -role hub -port 5555 &
---
+++ scripts/nodestart.bat
@@ -0,0 +1,1 @@
+java -jar src/test/java/resources/drivers/standalone/selenium-server-standalone-3.141.59.jar -role node  -hub http://localhost:5555/grid/register -nodeConfig node1Config.json &
